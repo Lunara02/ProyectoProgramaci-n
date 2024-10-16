857e3eb3d713c80a7aea106f58a44db59ce0a6af
Mazulini <matijenven2004@gmail.com>
2024-10-16 17:42:36 -0300

    Añade clase button, con constructor y propiedades.

---
+++ INTERFAZ/button.py
@@ -0,0 +1,10 @@
+import pygame
+import mouse_event
+
+
+class Button:
+    def __init__(self, x, y, image):
+        self.image = image
+        self.rect = image.get_rect()
+        self.rect.topleft = (x, y)
+        self.pressed = False
