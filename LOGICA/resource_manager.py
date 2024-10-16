b0fc7b2a4656bb7c5eb6d7ca7dc067d4d673db31
brai44 <braian.urba@gmail.com>
2024-10-16 16:45:55 -0300

    feat: Obtener path de las imagenes

--- LOGICA/resource_manager.py
+++ LOGICA/resource_manager.py
@@ -10,8 +10,16 @@ class ResourceManager:
     @staticmethod
     def level_path(file_name):
         return os.path.join(ResourceManager.levels_folder, file_name)
-    def image_load(self):
-        None
+
+    @staticmethod
+    def image_load(file_name):
+        get_image_path = os.path.join(ResourceManager.resources_folder, file_name)
+        try:
+            return pygame.image.load(get_image_path)
+        except pygame.error as e:
+            print(f"No se pudo cargar la imagen {file_name}: {e}")
+            return None
+
     def sound_load(self):
         None
     def music_load(self):
