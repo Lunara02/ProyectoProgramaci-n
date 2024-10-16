c8ee35625315f6e7507ff1810d5e62637cf34371
brai44 <braian.urba@gmail.com>
2024-10-16 17:22:46 -0300

    feat: Implementar funcion get_num en num_matriz

--- test/test_num_matriz.py
+++ test/test_num_matriz.py
@@ -1,8 +1,10 @@
 import unittest
-from LOGICA import num_matriz
+from LOGICA.num_matriz import GenNum
+
 
 class TestNumMatriz(unittest.TestCase):
     def test_get_num(self):
+        self.genNum = GenNum([])
         self.assertEqual(self.genNum.get_num([0, 1, 1, 1, 0]), [3])
         self.assertEqual(self.genNum.get_num([1, 1, 1, 1, 1]), [5])
         self.assertEqual(self.genNum.get_num([1, 0, 1, 0, 1]), [1, 1, 1])
