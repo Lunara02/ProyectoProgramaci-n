c29e0793004075d964738755fe655246ef9c18a0
brai44 <braian.urba@gmail.com>
2024-10-16 16:20:26 -0300

    Implementación rellenar bloque tablero

--- LOGICA/board.py
+++ LOGICA/board.py
@@ -5,6 +5,7 @@ class Board:
         self.n = n
         self.board = np.zeros((n,n), dtype = int)
     def fill_bloc(self, i, j):
+        self.board[i][j] = 1
 
     def remove_block(self, i,j):
 
