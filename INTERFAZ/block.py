0cf2f83b2fdd9100caf9a06ae8094d8cb955bbff
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2024-10-16 18:58:51 -0300

    Implementa manejo de eventos de clic para los bloques

--- INTERFAZ/block.py
+++ INTERFAZ/block.py
@@ -13,4 +13,12 @@ class Block:
         self.rect = None
         self.face = None
 
-        
+    def handle_event(self, events):
+        if mouse_event.left_click(events):
+            if self.face.collidepoint(pygame.mouse.get_pos()):
+                self.state = 1
+                return 1
+        elif mouse_event.right_click(events):
+            if self.face.collidepoint(pygame.mouse.get_pos()):
+                self.state = 0
+                return 0
