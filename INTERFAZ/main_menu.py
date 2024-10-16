2c34491981c20c49ed841b8ed74974e51fd5fcc9
Mazulini <matijenven2004@gmail.com>
2024-10-16 18:04:30 -0300

    Implementa metodo draw a la clase MainMenu
    - Implementa el metodo draw para llenar la superficie con color azul
    - Renderiza y dibuja el título 'Menu Principal' en la superficie
    - Dibuja el botón de dificultad en la superficie

--- INTERFAZ/main_menu.py
+++ INTERFAZ/main_menu.py
@@ -7,3 +7,9 @@ class MainMenu():
     def __init__(self):
         self.title_font = pygame.font.Font(None, 74)
         self.diff_button = Button(300, 300, GestorRecursos.image_load('icon.png'))
+
+    def draw(self, surface):
+        surface.fill(Color.AZUL)
+        title = self.title_font.render("Menu Principal", True, Color.BLANCO)
+        surface.blit(title, (100, 100))
+        self.diff_button.draw(surface)
