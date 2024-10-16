40978d95a0c2b3773de4ad5f2040bd5978f8d0c8
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2024-10-16 18:45:33 -0300

    Implementa la función draw
    Renderiza el tablero

--- INTERFAZ/game.py
+++ INTERFAZ/game.py
@@ -20,3 +20,11 @@ class GameScreen():
         for i in range(len(self.game.sol_board)):
             for j in range(len(self.game.sol_board)):
                 self.blocks.append(Block(640 + (blocks_size - 1) * j - int(blocks_size * self.n / 2 - 1 * (self.n - 1)), 360 + i * (blocks_size - 1) - int(blocks_size * self.n/2 - 1 * (self.n - 1)), int(500 / self.n)))
+
+    def draw(self, surface):
+        surface.fill((255, 255, 255))
+        for i in range(self.n):
+            for j in range(self.n):
+                self.blocks[i * self.n + j].draw(surface)
+
+        pygame.display.flip()
