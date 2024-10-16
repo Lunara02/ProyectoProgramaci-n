e61a82c4bc5574fa3b69463cbcb6ab2bc06f722c
Mazulini <matijenven2004@gmail.com>
2024-10-16 18:15:20 -0300

    Implementa metodo draw a la clase Difficult.
    - Implementa el método draw para llenar la superficie con un color celeste oscuro
    - Renderiza y dibuja el título 'Dificultad' en la superficie
    - Dibuja el botón de nivel en la superficie

--- INTERFAZ/difficulty.py
+++ INTERFAZ/difficulty.py
@@ -9,3 +9,9 @@ class Difficult():
     def __init__(self):
         self.title_font = pygame.font.Font(None, 74)
         self.level_button = Button(300, 300, GestorRecursos.image_load('icon.png'))
+
+    def draw(self, surface):
+        surface.fill((0, 100, 100))
+        title = self.title_font.render("Dificultad", True, Color.BLANCO)
+        surface.blit(title, (100, 100))
+        self.level_button.draw(surface)
