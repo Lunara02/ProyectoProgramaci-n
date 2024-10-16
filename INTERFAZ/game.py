fdda2f5aa2f8ec942ae667c9294d7a094c8d3314
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2024-10-16 18:43:33 -0300

    Implementa función def_level
    Carga los niveles

--- INTERFAZ/game.py
+++ INTERFAZ/game.py
@@ -9,3 +9,14 @@ class GameScreen():
         self.blocks = None
         self.cell_size = 50
         self.n = None
+
+    def def_level(self, index):
+        self.blocks = []
+        self.game = Nonograma()
+        self.game.load_level(index)
+        self.game.set_player_board()
+        self.n = len(self.game.sol_board)
+        blocks_size = int(500 / self.n)
+        for i in range(len(self.game.sol_board)):
+            for j in range(len(self.game.sol_board)):
+                self.blocks.append(Block(640 + (blocks_size - 1) * j - int(blocks_size * self.n / 2 - 1 * (self.n - 1)), 360 + i * (blocks_size - 1) - int(blocks_size * self.n/2 - 1 * (self.n - 1)), int(500 / self.n)))
