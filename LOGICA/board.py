cc9cd4220fd08569174fcf30c9476e45476b2a44
brai44 <braian.urba@gmail.com>
2024-10-16 16:15:36 -0300

    Creación métodos de tablero

---
+++ LOGICA/board.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class Board:
+    def __init__(self,n):
+
+    def fill_bloc(self, i, j):
+
+    def remove_block(self, i,j):
+
+    def reset(self):
+
+    def print_board(self):
