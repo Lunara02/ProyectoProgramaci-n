1a43718f2f5c53f2d6d4f3f1c4ac738901477b36
Lunara02 <cristobagonzalez2023@inf.udec.cl>
2024-10-16 18:57:45 -0300

    test: añadido caso matriz vacia

--- test/test_num_matriz.py
+++ test/test_num_matriz.py
@@ -3,6 +3,15 @@ from LOGICA.num_matriz import GenNum
 
 
 class TestNumMatriz(unittest.TestCase):
+    def setUp(self):
+        self.matriz = [
+            [0, 1, 1, 1, 0],
+            [1, 1, 1, 1, 1],
+            [1, 0, 1, 0, 1],
+            [0, 1, 1, 1, 0],
+            [0, 0, 1, 0, 0]
+        ]
+        self.genNum = GenNum(self.matriz)
 
     def test_get_num(self):
         self.genNum = GenNum([])
@@ -25,3 +34,9 @@ class TestNumMatriz(unittest.TestCase):
 
         self.assertEqual(self.genNum.num_filas, [[3], [5], [1,1,1], [3], [1]])
         self.assertEqual(self.genNum.num_columnas, [[2], [2,1], [5], [2,1], [2]])
+
+    def test_matriz_vacia(self):
+        matriz_vacia = []
+        gen_num_vacio = GenNum(matriz_vacia)
+        self.assertEqual(gen_num_vacio.num_filas, [])
+        self.assertEqual(gen_num_vacio.num_columnas, [])
