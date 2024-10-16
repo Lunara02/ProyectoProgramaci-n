2f820e5ba8b7961c14af36730f79991e34e01b93
Lunara02 <cristobagonzalez2023@inf.udec.cl>
2024-10-16 09:29:07 -0300

    Agregar métodos iniciales para Nonograma (implementaciones vacías)

---
+++ LOGICA/nonograma.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+class Nonograma:
+    def __init__(self, n):
+        self.player_board = np.zeros((n, n), dtype=int)
+        self.sol_board = np.zeros((n, n), dtype=int)
+
+    def get_sol(self):
+        return
+
+    def get_player(self):
+        return
+
+    def set_sol(self):
+        return
+
+    def set_player(self, save):
+        return
+
+    def win_condition(self):
+        return
+
+    def load_level(self, level):
+        return
