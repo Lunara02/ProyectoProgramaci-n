c1b39dde9329a3fac76119cf0cdfe8df68573097
Mazulini <matijenven2004@gmail.com>
2024-10-16 18:23:30 -0300

    Implementa metodo draw a la clase Levels
    - Implementa el método draw para llenar la superficie con un color purpura
    - Renderiza y dibuja el título 'Niveles' en la superficie
    - Dibuja los botones de nivel en la superficie

--- INTERFAZ/levels.py
+++ INTERFAZ/levels.py
@@ -13,3 +13,11 @@ class Levels():
         self.level_2 = Button(100, 300, button_image)
         self.level_3 = Button(100, 400, button_image)
         self.list_levels = [self.level_1, self.level_2, self.level_3]
+
+    def draw(self, surface):
+        surface.fill((100, 0, 100))
+        title = self.title_font.render("Niveles", True, Color.BLANCO)
+        surface.blit(title, (100, 100))
+
+        for button in self.list_levels:
+            button.draw(surface)
