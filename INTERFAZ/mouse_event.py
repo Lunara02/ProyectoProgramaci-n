c26be259093f0c7a45ddeecee06f0ded97f81bc3
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2024-10-16 18:52:03 -0300

    Añade función para detectar clics derecho del mouse

--- INTERFAZ/mouse_event.py
+++ INTERFAZ/mouse_event.py
@@ -6,3 +6,10 @@ def left_click(events):
             if event.button == 1:
                 return True
     return False
+
+def right_click(events):
+    for event in events:
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 3:
+                return True
+    return False
