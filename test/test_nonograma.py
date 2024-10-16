ff316aa929092dda3e57fb4a27bdf2efeefc7094
Lunara02 <cristobagonzalez2023@inf.udec.cl>
2024-10-16 09:30:04 -0300

    Agregar pruebas unitarias para los métodos de la clase Nonograma

---
+++ test/test_nonograma.py
@@ -0,0 +1,43 @@
+import unittest
+import numpy as np
+from ProyectoProgramacion.LOGICA.nonograma import Nonograma
+
+
+class TestNon(unittest.TestCase):
+
+    def setUp(self):
+        self.game = Nonograma(5)
+        self.test_board = np.array([
+            [1, 0, 1, 0, 1],
+            [0, 1, 0, 1, 0],
+            [1, 1, 0, 0, 1],
+            [0, 1, 1, 1, 0],
+            [1, 0, 0, 0, 1]
+        ])
+
+    def test_get_sol(self):
+        self.assertEqual(self.game.get_sol(), self.game.sol_board)
+
+    def test_get_player(self):
+        self.assertEqual(self.game.get_player(), self.game.player_board)
+
+    def test_set_sol(self):
+        self.game.set_sol(self.test_board)
+        self.assertEqual(self.test_board, self.game.sol_board)
+
+    def test_set_player(self):
+
+        self.game.set_player(self.test_board)
+        self.assertEqual(self.test_board, self.game.player_board)
+
+    def test_win_condition(self):
+        self.game.player_board = self.game.sol_board
+        self.assertEqual(self.game.win_condition(), True)
+
+    def test_load_level(self):
+        self.game.sol_board = self.test_board
+        self.assertEqual(self.game.sol_board, self.game.load_level(self.test_board))
+
+
+if __name__ == '__main__':
+    unittest.main()
