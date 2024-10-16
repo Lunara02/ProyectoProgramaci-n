a933928e336e3462a0ad6155c5ebc11bfa3fc1ec
brai44 <braian.urba@gmail.com>
2024-10-16 16:41:34 -0300

    Implementación obtener directorio principal

--- LOGICA/resource_manager.py
+++ LOGICA/resource_manager.py
@@ -2,7 +2,15 @@ import os
 import pygame
 
 class ResourceManager:
+    proyect_folder = os.path.dirname(__file__)
+
+    resources_folder = os.path.join(os.path.dirname(proyect_folder), 'resources')
+    levels_folder = os.path.join(os.path.dirname(proyect_folder), 'levels')
     def level_path(self):
+        None
     def image_load(self):
+        None
     def sound_load(self):
+        None
     def music_load(self):
+        None
