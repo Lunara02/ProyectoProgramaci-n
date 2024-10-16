97437542472822cb1eb2bfaa4f2c1aaa8f51e160
brai44 <braian.urba@gmail.com>
2024-10-16 16:22:44 -0300

    Implementación desmarcar bloque

--- LOGICA/board.py
+++ LOGICA/board.py
@@ -4,10 +4,12 @@ class Board:
     def __init__(self,n):
         self.n = n
         self.board = np.zeros((n,n), dtype = int)
+
     def fill_bloc(self, i, j):
         self.board[i][j] = 1
 
     def remove_block(self, i,j):
+        self.board[i][j] = 0
 
     def reset(self):
 
