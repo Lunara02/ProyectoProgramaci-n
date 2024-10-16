266d45fbd0c68aabbd40dbc0862116a9f298916b
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2024-10-16 18:54:09 -0300

    Implementa manejo de eventos y cambio de pantallas

--- INTERFAZ/screen_manager.py
+++ INTERFAZ/screen_manager.py
@@ -13,3 +13,13 @@ class ScreenManager:
             'game': GameScreen()
         }
         self.current_screen = 'main_menu'
+
+    def handle_events(self, events):
+        result = self.screens[self.current_screen].handle_events(events)
+        if result:
+            if isinstance(result, tuple):
+                next_screen, level_index = result
+                self.screens['game'].def_level(level_index)
+                self.current_screen = next_screen.lower()
+            else:
+                self.current_screen = result.lower()
