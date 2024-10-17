256acc780e7aa7ab46bb50d049f5bd2103619a34
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2024-10-16 21:41:40 -0300

    Añade GenNum en nonoframa

--- LOGICA/nonograma.py
+++ LOGICA/nonograma.py
@@ -1,11 +1,13 @@
 import numpy as np
 import os
+from .num_matriz import GenNum
 from .resource_manager import ResourceManager
 
 class Nonograma:
     def __init__(self):
         self.player_board = None
         self.sol_board = None
+        self.sol_matriz = None
 
     def get_sol(self):
         return self.sol_board
@@ -42,6 +44,7 @@ class Nonograma:
             if int(id_actual) == id_matriz:
                 ruta_archivo_matriz = os.path.join(carpeta_base, archivo_matriz)
                 self.sol_board = np.loadtxt(ruta_archivo_matriz, dtype=int)
+                self.sol_matriz = GenNum(self.sol_board)
                 self.player_board = np.zeros(self.sol_board.shape, dtype=int)
                 return
 
