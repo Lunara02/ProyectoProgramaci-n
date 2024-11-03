ba4e8c7291a4244663d1c1631cd5f15aab85514d
brai44 <braian.urba@gmail.com>
2024-11-02 23:00:39 -0300

    Implementar guardado

--- INTERFAZ/game.py
+++ INTERFAZ/game.py
@@ -13,11 +13,13 @@ class GameScreen():
         self.num_filas = None
         self.num_columnas = None
         self.blocks_size = None
+        self.index = None
 
     def def_level(self, index):
         self.blocks = []
         self.game = Nonograma()
-        self.game.load_level(index)
+        self.index = index
+        self.game.load_level(self.index)
         self.num_filas = self.game.sol_matriz.num_filas
         self.num_columnas = self.game.sol_matriz.num_columnas
         self.n = len(self.game.sol_board)
@@ -25,10 +27,11 @@ class GameScreen():
         for i in range(len(self.game.sol_board)):
             for j in range(len(self.game.sol_board)):
                 self.blocks.append(Block(640 + (self.blocks_size - 1) * j - int(self.blocks_size * self.n / 2 - 1 * (self.n - 1)), 360 + i * (self.blocks_size - 1) - int(self.blocks_size * self.n/2 - 1 * (self.n - 1)), int(500 / self.n)))
+                if self.game.player_board[i][j]==1:
+                    self.blocks[i * self.n + j].state=1
 
     def draw(self, surface):
         surface.fill((255, 255, 255))
-
         for i in range(self.n):
             for j in range(self.n):
                 self.blocks[i * self.n + j].draw(surface)
@@ -58,6 +61,14 @@ class GameScreen():
                     self.game.fill_box(i, j)
                 if self.blocks[i * self.n + j].handle_event(events) == 0:
                     self.game.empty_box(i, j)
+
+        for event in events:
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_s:
+                    self.game.save_level(self.index)
+                if event.key == pygame.K_d:
+                    self.game.wipe_saved(self.index)
+
 
         if self.game.win_condition():
             self.blocks = None
