cff0f8033dc594274dcc681e0907ce3603953be5
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2024-11-03 21:42:49 -0300

    feat nuevos nivels + exit

--- INTERFAZ/levels.py
+++ INTERFAZ/levels.py
@@ -1,8 +1,9 @@
 import pygame
 
-from LOGICA.resource_manager import ResourceManager
+from INTERFAZ.resource_manager import ResourceManager
 from button import Button
 import Color
+import input_event
 
 
 class Levels():
@@ -12,13 +13,23 @@ class Levels():
         self.level_1 = Button(100, 200, button_image)
         self.level_2 = Button(100, 300, button_image)
         self.level_3 = Button(100, 400, button_image)
-        self.list_levels = [self.level_1, self.level_2, self.level_3]
+        self.level_4 = Button(100, 500, button_image)
+        self.level_5 = Button(100, 600, button_image)
+        self.level_6 = Button(400, 200, button_image)
+        self.level_7 = Button(400, 300, button_image)
+        self.level_8 = Button(400, 400, button_image)
+        self.level_9 = Button(400, 500, button_image)
+        self.level_10 = Button(400, 600, button_image)
+
+        self.list_levels = [self.level_1, self.level_2, self.level_3, self.level_4, self.level_5,
+                            self.level_6, self.level_7, self.level_8, self.level_9, self.level_10]
+        self.exit = Button(980, 640, ResourceManager.image_load('icon.png'))
 
     def draw(self, surface):
         surface.fill((100, 0, 100))
         title = self.title_font.render("Niveles", True, Color.BLANCO)
         surface.blit(title, (100, 100))
-
+        self.exit.draw(surface)
         for button in self.list_levels:
             button.draw(surface)
 
@@ -26,4 +37,6 @@ class Levels():
         for index, button in enumerate(self.list_levels, start=1):
             if button.click_event(events):
                 return 'game', index
+        if self.exit.click_event(events):
+            return 'main_menu'
         return None
