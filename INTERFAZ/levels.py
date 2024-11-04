ad537f6f659111b3058eb247a06820b712ed404d
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2024-11-03 22:32:28 -0300

    refactor botones

--- INTERFAZ/levels.py
+++ INTERFAZ/levels.py
@@ -9,26 +9,26 @@ import input_event
 class Levels():
     def __init__(self):
         self.title_font = pygame.font.Font(None, 74)
-        button_image = ResourceManager.image_load('icon.png')
+        button_image = ResourceManager.image_load('bot.png')
         self.level_1 = Button(100, 200, button_image)
-        self.level_2 = Button(100, 300, button_image)
-        self.level_3 = Button(100, 400, button_image)
-        self.level_4 = Button(100, 500, button_image)
-        self.level_5 = Button(100, 600, button_image)
-        self.level_6 = Button(400, 200, button_image)
-        self.level_7 = Button(400, 300, button_image)
-        self.level_8 = Button(400, 400, button_image)
-        self.level_9 = Button(400, 500, button_image)
-        self.level_10 = Button(400, 600, button_image)
-
+        self.level_2 = Button(250, 200, button_image)
+        self.level_3 = Button(400, 200, button_image)
+        self.level_4 = Button(550, 200, button_image)
+        self.level_5 = Button(700, 200, button_image)
+        self.level_6 = Button(850, 200, button_image)
+        self.level_7 = Button(700, 400, button_image)
+        self.level_8 = Button(800, 400, button_image)
+        self.level_9 = Button(900, 400, button_image)
+        self.level_10 = Button(1000, 400, button_image)
         self.list_levels = [self.level_1, self.level_2, self.level_3, self.level_4, self.level_5,
-                            self.level_6, self.level_7, self.level_8, self.level_9, self.level_10]
+                            self.level_6]
         self.exit = Button(980, 640, ResourceManager.image_load('icon.png'))
 
     def draw(self, surface):
-        surface.fill((100, 0, 100))
-        title = self.title_font.render("Niveles", True, Color.BLANCO)
-        surface.blit(title, (100, 100))
+        surface.fill((Color.BLANCO))
+        title = self.title_font.render("Niveles", True, Color.NEGRO)
+        title_rect = title.get_rect(center=(surface.get_width() // 2, 100))
+        surface.blit(title, title_rect.topleft)
         self.exit.draw(surface)
         for button in self.list_levels:
             button.draw(surface)
