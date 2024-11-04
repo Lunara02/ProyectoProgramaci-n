6f63329ce3c98a99ed6d57d9681b52469be6e7d0
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2024-11-03 21:40:20 -0300

    refactor input_event

--- INTERFAZ/button.py
+++ INTERFAZ/button.py
@@ -1,19 +1,21 @@
 import pygame
-import mouse_event
+import input_event
 
 
 class Button:
     def __init__(self, x, y, image):
         self.image = image
         self.rect = image.get_rect()
-        self.rect.topleft = (x, y)
+        self.x = x
+        self.y = y
         self.pressed = False
 
     def draw(self, surface):
+        self.rect.topleft = (self.x, self.y)
         surface.blit(self.image, self.rect.topleft)
 
     def click_event(self, events):
-        if mouse_event.left_click(events):
+        if input_event.left_click(events):
             if self.rect.collidepoint(pygame.mouse.get_pos()):
                 return True
         return False
