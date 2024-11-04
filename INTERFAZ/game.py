1c620888288a17c4121f57b066e576529a4d196e
Sebavegs2004 <167146245+Sebavegs2004@users.noreply.github.com>
2024-11-03 21:48:45 -0300

    feat menu de pausa + guardar niveles + refactor
    se implementa el menu de pausa y se modifica el codigo para poder guardar niveles tambien se refactoriza la forma en que se maneja los eventos de bloques

--- INTERFAZ/game.py
+++ INTERFAZ/game.py
@@ -1,76 +1,99 @@
+from Xlib.Xcursorfont import mouse
+
+from INTERFAZ.resource_manager import ResourceManager
 from LOGICA.nonograma import Nonograma
 from block import Block
 from button import Button
 import Color
+import input_event
 import pygame
+from  pause_menu import PauseMenu
 
 class GameScreen():
     def __init__(self):
-        self.game = None
+        self.nonograma = Nonograma()
         self.blocks = None
-        self.cell_size = 50
         self.n = None
         self.num_filas = None
         self.num_columnas = None
         self.blocks_size = None
+        self.mouse_action = 0
         self.index = None
+        self.pause_menu = PauseMenu(500, 170, self.nonograma)
 
     def def_level(self, index):
         self.blocks = []
-        self.game = Nonograma()
         self.index = index
-        self.game.load_level(self.index)
-        self.num_filas = self.game.sol_matriz.num_filas
-        self.num_columnas = self.game.sol_matriz.num_columnas
-        self.n = len(self.game.sol_board)
+        self.nonograma.load_level(index)
+        self.num_filas = self.nonograma.sol_matriz.num_filas
+        self.num_columnas = self.nonograma.sol_matriz.num_columnas
+        self.n = len(self.nonograma.sol_board)
         self.blocks_size = int(500 / self.n)
-        for i in range(len(self.game.sol_board)):
-            for j in range(len(self.game.sol_board)):
+        for i in range(len(self.nonograma.sol_board)):
+            for j in range(len(self.nonograma.sol_board)):
                 self.blocks.append(Block(640 + (self.blocks_size - 1) * j - int(self.blocks_size * self.n / 2 - 1 * (self.n - 1)), 360 + i * (self.blocks_size - 1) - int(self.blocks_size * self.n/2 - 1 * (self.n - 1)), int(500 / self.n)))
-                if self.game.player_board[i][j]==1:
-                    self.blocks[i * self.n + j].state=1
+                self.blocks[i * self.n + j].state = self.nonograma.player_board[i][j]
+        print(self.num_filas)
 
     def draw(self, surface):
         surface.fill((255, 255, 255))
+        xprop = surface.get_size()[0] / 1280
+        yprop = surface.get_size()[1] / 720
+        if xprop > yprop:
+            font = pygame.font.Font(None, int(self.blocks_size * yprop - 7))
+        else:
+            font = pygame.font.Font(None, int(self.blocks_size * xprop - 7))
+
+        dimensions = pygame.font.Font(None, 50)
+        dim = dimensions.render(str(self.n) + ' x ' + str(self.n), True, Color.NEGRO)
+        surface.blit(dim, (200, 50))
+
         for i in range(self.n):
+            for fila in range(len(self.num_filas[i])):
+                text_surface = font.render(str((self.num_filas[i][::-1][fila])), True, Color.NEGRO)
+                surface.blit(text_surface, (int((600 - 25 * fila - int(self.blocks_size * self.n / 2 - 1 * (self.n - 1))) * xprop), int(374 + i * (self.blocks_size - 1) - int(self.blocks_size * self.n/2 - 1 * (self.n - 1))) * yprop))
             for j in range(self.n):
+                for col in range(len(self.num_columnas[j])):
+                    text_surface = font.render(str(self.num_columnas[j][::-1][col]), True, Color.NEGRO)
+                    surface.blit(text_surface, (int((654 + (self.blocks_size - 1) * j - int(self.blocks_size * self.n / 2 - 1 * (self.n - 1))) * xprop), int(330 - 20 * col - int(self.blocks_size * self.n/2 - 1 * (self.n - 1))) * yprop))
                 self.blocks[i * self.n + j].draw(surface)
 
-        font = pygame.font.Font(None, 36)
-
-        for filas in range(len(self.num_filas)):
-            for u in range(len(self.num_filas[filas])):
-                text_surface = font.render(str((self.num_filas[filas][::-1])[u]), True, Color.NEGRO)
-                text_rect = text_surface.get_rect()
-                text_rect.center = (u*-25+80,filas*56+130)
-                surface.blit(text_surface, text_rect)
-
-        for columnas in range(len(self.num_columnas)):
-            for v in range(len(self.num_columnas[columnas])):
-                text_surface = font.render(str((self.num_columnas[columnas][::-1])[v]), True, Color.NEGRO)
-                text_rect = text_surface.get_rect()
-                text_rect.center = (columnas*self.blocks_size+130,v*-25+80)
-                surface.blit(text_surface, text_rect)
+        if self.pause_menu.state == 1:
+            self.pause_menu.draw(surface)
 
         pygame.display.flip()
 
     def handle_events(self, events):
-        for i in range(self.n):
-            for j in range(self.n):
-                if self.blocks[i * self.n + j].handle_event(events) == 1:
-                    self.game.fill_box(i, j)
-                if self.blocks[i * self.n + j].handle_event(events) == 0:
-                    self.game.empty_box(i, j)
+        if input_event.esc_press(events):
+            self.pause_menu.set_state()
 
-        for event in events:
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_s:
-                    self.game.save_level(self.index)
-                if event.key == pygame.K_d:
-                    self.game.wipe_saved(self.index)
+        if self.pause_menu.state == 0:
+            for i in range(self.n):
+                for j in range(self.n):
+                    if input_event.left_click(events) and self.blocks[i * self.n + j].collide():
+                        if self.nonograma.player_board[i][j] == 1:
+                            self.mouse_action = 0
+                        else:
+                            self.mouse_action = 1
+                    if input_event.right_click(events) and self.blocks[i * self.n + j].collide():
+                        if self.nonograma.player_board[i][j] == 2:
+                            self.mouse_action = 0
+                        else:
+                            self.mouse_action = 2
+                    if input_event.left_hold() and self.blocks[i * self.n + j].collide():
+                        self.nonograma.set_box_value(i, j, self.mouse_action)
+                        self.blocks[i * self.n + j].state_change(self.nonograma.player_board[i][j])
+                    if input_event.right_hold() and self.blocks[i * self.n + j].collide():
+                        self.nonograma.set_box_value(i, j, self.mouse_action)
+                        self.blocks[i * self.n + j].state_change(self.nonograma.player_board[i][j])
+        else:
+            result = self.pause_menu.handle_events(events, self.index)
+            if result:
+                self.pause_menu.state = 0
+                return result
 
-
-        if self.game.win_condition():
+        if self.nonograma.win_condition():
             self.blocks = None
-            self.game.wipe()
+            self.nonograma.wipe()
+            self.nonograma.wipe_saved(self.index)
             return 'levels'
